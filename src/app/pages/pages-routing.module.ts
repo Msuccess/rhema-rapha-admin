80dca863d021c7682f1d8ebfdf5e703c4484e3db
MosesNkrumah <moses.nkrumah@scynett-ghana.com>
2020-10-21 18:04:37 +0000

    new image

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,35 +3,33 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-    { path: '', component: DashboardComponent },
-    {
-        path: 'appointment',
-        loadChildren: () =>
-            import('./appointment/appointment.module').then(
-                (m) => m.AppointmentModule
-            ),
-    },
-    {
-        path: 'department',
-        loadChildren: () =>
-            import('./department/department.module').then(
-                (m) => m.DepartmentModule
-            ),
-    },
-    {
-        path: 'doctor',
-        loadChildren: () =>
-            import('./doctor/doctor.module').then((m) => m.DoctorModule),
-    },
-    {
-        path: 'patient',
-        loadChildren: () =>
-            import('./patient/patient.module').then((m) => m.PatientModule),
-    },
+  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'appointment',
+    loadChildren: () =>
+      import('./appointment/appointment.module').then(
+        (m) => m.AppointmentModule,
+      ),
+  },
+  {
+    path: 'department',
+    loadChildren: () =>
+      import('./department/department.module').then((m) => m.DepartmentModule),
+  },
+  {
+    path: 'doctor',
+    loadChildren: () =>
+      import('./doctor/doctor.module').then((m) => m.DoctorModule),
+  },
+  {
+    path: 'patient',
+    loadChildren: () =>
+      import('./patient/patient.module').then((m) => m.PatientModule),
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
 export class PagesRoutingModule {}
